c1f19e0170ebcb39be8e62c291011cb5152331e4
Jahn <ioannis.kiwe@gmail.com>
2014-08-30 00:48:39 +0200

    hermes-bus.js: Refactored design of bus. Discarded emit function. Now the event automaticaly is transformed to module function and can be invoked as bus.busline.event or if its in the main busline bus.event

--- hermes-bus.js
+++ hermes-bus.js
@@ -1,6 +1,6 @@
 
 var observerBusLines = {
-    default: []
+    main: []
 };
 
 function createObserver(context, event, cb) {
@@ -12,30 +12,73 @@ function createObserver(context, event, cb) {
     }
 }
 
-function emit(event, data) {
-    var busline = getBusline(event);
-    for (var i = 0; i <= observerBusLines[busline].length - 1; i++) {
-        if (observerBusLines[busline][i].event === event && observerBusLines[busline][i].active === true) {
-            observerBusLines[busline][i].cb(data);
-        }
-    }
-}
 
 function getBusline(eventMsg) {
     var busline = eventMsg.split('-')[0];
     if (busline === "" || busline[0] !== '#') {
-        busline = "default";
+        busline = "#main";
     }
-    return busline;
+    return busline.slice(1);
 }
+
 
 function onEvent(context, event, cb) {
     var busline = getBusline(event);
+    if (busline !== "main") {
+        event = event.split('-')[1];
+    }
     if (observerBusLines[busline]) {
         observerBusLines[busline].push(createObserver(context, event, cb));
+        eventHandlerBuilder(this, getNamespaceInfo(busline, event));
     }
     else {
-        observerBusLines[busline] = [createObserver(context, event, cb)]
+        observerBusLines[busline] = [createObserver(context, event, cb)];
+        eventHandlerBuilder(this, getNamespaceInfo(busline, event));
+    }
+}
+
+
+function getNamespaceInfo(busline, event) {
+    var infoHolder = {};
+    infoHolder.busline = busline;
+    infoHolder.event = event;
+    infoHolder.namespaceElements = []
+
+    if (busline === "main") {
+        infoHolder.namespaceElements.push(event);
+    } else {
+        infoHolder.namespaceElements = [busline, event];
+    }
+
+    return infoHolder;
+}
+
+function eventHandlerBuilder(moduleContext, namespaceInfos) {
+    var exportsBuilder = exports;
+    var namespaceElements = namespaceInfos.namespaceElements;
+    
+    function createNamespace(context, element) {
+        if (namespaceElements.length === 0) {
+            context[element] = getEventHandler(namespaceInfos.busline, namespaceInfos.event);
+            exportsBuilder[element] = context[element];
+        } else {
+            if (!context[element]) {
+                context[element] = {};
+            }
+            exportsBuilder = exportsBuilder[element];
+            createNamespace(context[element], namespaceElements.shift());
+        }
+    }
+    createNamespace(moduleContext, namespaceElements.shift())
+}
+
+function getEventHandler(busline, event) {
+    return function(data) {
+        for (var i = 0; i <= observerBusLines[busline].length - 1; i++) {
+            if (observerBusLines[busline][i].event === event && observerBusLines[busline][i].active === true) {
+                observerBusLines[busline][i].cb(data);
+            }
+        }
     }
 }
 
@@ -63,7 +106,6 @@ function reactivate(context) {
 }
 
 
-exports.emit = emit;
 exports.onEvent = onEvent;
 exports.reactivate = reactivate;
 exports.deactivate = deactivate;
