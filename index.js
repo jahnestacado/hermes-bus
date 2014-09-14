a2de6c156aad695b13636b3eb32a3ec7da732136
Jahn <ioannis.kiwe@gmail.com>
2014-09-15 00:16:54 +0200

    index.js: Added license header

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/**
+ * hermes-bus <https://github.com/jahnestacado/hermes-bus>
+ * Copyright (c) 2014 Ioannis Tzanellis
+ * Licensed under the MIT License (MIT).
+ */
+
 module.exports = require('./lib/hermes-bus.js');
