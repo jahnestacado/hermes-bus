787a45e6a8f6561a4c6d90036a9804b222d74934
Jahn <ioannis.kiwe@gmail.com>
2014-10-15 23:25:42 +0200

    test/hermes-bus.js:Wrote tests for 'reset' and 'hardReset' and did some refactoring

--- test/hermes-bus.js
+++ test/hermes-bus.js
@@ -61,7 +61,10 @@ describe('#################### Start integration tests for hermes-bus module \n'
 
                 describe('create "red busline" ', function() {
                     var thirdEventCallback = sinon.spy();
-                    bus.onEvent("red", "firstEvent", thirdEventCallback);
+                  
+                    before(function() {
+                        bus.onEvent("red", "firstEvent", thirdEventCallback);
+                    });
 
                     describe('invocation of bus.red.emitFirstEvent', function() {
                         var thirdEventArg1 = {isDummy: true};
@@ -126,33 +129,71 @@ describe('#################### Start integration tests for hermes-bus module \n'
 
                             });
 
-                            describe('invoke destroy on "red" busline', function() {
+                            describe('create "green" and "black" and "white" buslines ', function() {
 
                                 before(function() {
-                                    bus.red.destroy();
+                                    bus.onEvent("green", "dummyEvent", function() {});
+                                    bus.onEvent("black", "dummyEvent", function() {});
                                 });
 
-                                it('should destroy "red" busline ', function() {
-                                    assert.equal(bus.red, undefined);
+                                it('should have "green" busline ', function() {
+                                    assert(bus.green);
                                 });
 
+                                it('should have "black" busline ', function() {
+                                    assert(bus.black);
+                                });
 
-                                describe('invoke destroy on "main" busline', function() {
+                                describe('invoke reset on "main" busline', function() {
 
                                     before(function() {
-                                        bus.destroy();
+                                        bus.reset();
                                     });
 
-                                    it('should destroy emitFirstEvent on "main" busline ', function() {
+                                    it('should not have emitFirstEvent function on "main" busline ', function() {
                                         assert.equal(bus.emitFirstEvent, undefined);
                                     });
 
-                                    it('should destroy emitSecondEvent on "main" busline', function() {
+                                    it('should not have emitSecondEvent function on "main" busline', function() {
                                         assert.equal(bus.emitSecondEvent, undefined);
                                     });
 
-                                });
+                                    it('should maintain "red" busline ', function() {
+                                        assert(bus.red);
+                                    });
 
+                                    it('should maintain "green" busline ', function() {
+                                        assert(bus.green);
+                                    });
+
+                                    describe('invoke destroy on "red" busline', function() {
+
+                                        before(function() {
+                                            bus.red.destroy();
+                                        });
+
+                                        it('should destroy "red" busline ', function() {
+                                            assert.equal(bus.red, undefined);
+                                        });
+
+                                        describe('invoke hardReset on "main" busline', function() {
+
+                                            before(function() {
+                                                bus.hardReset();
+                                            });
+
+                                            it('should destroy "green" busline ', function() {
+                                                assert.equal(bus.green, undefined);
+                                            });
+
+                                            it('should destroy "white" busline ', function() {
+                                                assert.equal(bus.green, undefined);
+                                            });
+
+                                        });
+                                    });
+
+                                });
                             });
 
                         });
@@ -180,7 +221,6 @@ describe('#################### Start integration tests for hermes-bus module \n'
                 return a / b;
             }
 
-
             function getDelayedAsyncFunc(cb, delay) {
                 return    function asyncFunc(a, b, resolve) {
                     setTimeout(function() {
@@ -193,69 +233,71 @@ describe('#################### Start integration tests for hermes-bus module \n'
             var secondEventCallback = sinon.spy(getDelayedAsyncFunc(mulCb, getRandomInt(500)));
             var thirdEventCallback = sinon.spy(getDelayedAsyncFunc(divCb, getRandomInt(500)));
 
+            describe('register events', function() {
 
-
-            bus.onEvent("syncLine", "forthEvent", firstEventCallback);
-            bus.onEvent("syncLine", "forthEvent", secondEventCallback);
-            bus.onEvent("syncLine", "forthEvent", thirdEventCallback);
-
-            describe('invocation of bus.syncLine.resolveForthEvent with use of .then()', function() {
-                var a = 566;
-                var b = 5;
-                var results;
-                before(function(done) {
-                    bus.syncLine.resolveForthEvent(a, b)
-                            .then(function(output) {
-                                results = output;
-                                done();
-                            });
+                before(function() {
+                    bus.onEvent("syncLine", "forthEvent", firstEventCallback);
+                    bus.onEvent("syncLine", "forthEvent", secondEventCallback);
+                    bus.onEvent("syncLine", "forthEvent", thirdEventCallback);
                 });
 
-                it('should invoke firstEventCallback once', function() {
-                    assert(firstEventCallback.calledOnce);
-                });
+                describe('invocation of bus.syncLine.resolveForthEvent with use of .then()', function() {
+                    var a = 566;
+                    var b = 5;
+                    var results;
+                    before(function(done) {
+                        bus.syncLine.resolveForthEvent(a, b)
+                                .then(function(output) {
+                                    results = output;
+                                    done();
+                                });
+                    });
+
+                    it('should invoke firstEventCallback once', function() {
+                        assert(firstEventCallback.calledOnce);
+                    });
 
 
-                it('should invoke firstEventCallback once', function() {
-                    assert(secondEventCallback.calledOnce);
-                });
+                    it('should invoke firstEventCallback once', function() {
+                        assert(secondEventCallback.calledOnce);
+                    });
 
-                it('should invoke firstEventCallback once', function() {
-                    assert(thirdEventCallback.calledOnce);
-                });
+                    it('should invoke firstEventCallback once', function() {
+                        assert(thirdEventCallback.calledOnce);
+                    });
 
-                it("should invoke firstEventCallback with the right arguments", function() {
-                    assert(firstEventCallback.calledWith(a, b));
-                });
+                    it("should invoke firstEventCallback with the right arguments", function() {
+                        assert(firstEventCallback.calledWith(a, b));
+                    });
 
-                it("should invoke secondEventCallback with the right arguments", function() {
-                    assert(secondEventCallback.calledWith(a, b));
-                });
+                    it("should invoke secondEventCallback with the right arguments", function() {
+                        assert(secondEventCallback.calledWith(a, b));
+                    });
 
-                it("should invoke thirdEventCallback with the right arguments", function() {
-                    assert(thirdEventCallback.calledWith(a, b));
-                });
+                    it("should invoke thirdEventCallback with the right arguments", function() {
+                        assert(thirdEventCallback.calledWith(a, b));
+                    });
 
-                it('first element of "results" should be equal to 571', function() {
-                    assert.equal(results[0], 571);
-                });
+                    it('first element of "results" should be equal to 571', function() {
+                        assert.equal(results[0], 571);
+                    });
 
-                it('second element of "results" should be equal to 2830', function() {
-                    assert.equal(results[1], '2830');
-                });
+                    it('second element of "results" should be equal to 2830', function() {
+                        assert.equal(results[1], '2830');
+                    });
 
-                it('first element of "results" should be equal to 113.2', function() {
-                    assert.equal(results[2], '113.2');
+                    it('first element of "results" should be equal to 113.2', function() {
+                        assert.equal(results[2], '113.2');
+                    });
+
                 });
 
             });
 
         });
 
+        after(function() {
+            console.log("\n  #################### End of integration tests for hermes-bus module");
+        });
     });
-
-    after(function() {
-        console.log("\n  #################### End of integration tests for hermes-bus module");
-    });
-
 });
